70ff663a838e82a75538df4adf7638070be6d06d
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-03-09 18:51:35 +0300

    added demonstration version

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const app = express();
 // }
 // app.use(cors(corsOptions))
 
-// if (process.env.NODE_ENV === "production") {
-// 	// Serve any static files
-// 	app.use(express.static(path.join(__dirname, "client/build")));
-// 	// Handle React routing, return all requests to React app
-// 	app.get("*", function (req, res) {
-// 		res.sendFile(path.join(__dirname, "client/build", "index.html"));
-// 	});
-// }
+if (process.env.NODE_ENV === "production") {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, "client/build")));
+	// Handle React routing, return all requests to React app
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+	});
+}
 
 app.use("/api", require("./routes/calculation.routes"));
 
